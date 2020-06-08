7a221955971c8c8ede2074c46528b3047b1b1a97
martynab99 <66648402+martynab99@users.noreply.github.com>
2020-06-09 00:52:36 +0200

    Update 01program.php

--- 01program.php
+++ 01program.php
@@ -2,6 +2,11 @@
 echo ("Witaj");
 echo("<br> Jestem programem 01");
 echo ("<br> Test");
+echo ("<br> Test 1");
+echo ("<br> TeSt 2");
+echo ("<br> czaczacza");
+echo <"br> Dalsza czesc programu");
+echo <"br> Dalsza czesc programu 2222 z GitHuba");
 ?>
 
 
